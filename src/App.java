01ecae9ff377c519bb694e0e2d9ecfec45037c27
Mauricio <stb539@gmail.com>
2025-06-28 12:14:31 -0400

    total ventas

--- src/App.java
+++ src/App.java
@@ -8,6 +8,32 @@ public class App {
 
         double total; // 0.0
 
-        System.out.println("Hello, World!");// metodo estatico println de la clase System para imprimir en consola
+        System.out.println("Ventas diarias");// metodo estatico println de la clase System para imprimir en consola
+
+        double[] ventasSemana = new double[7]; // la longitud del array es 7, para los 7 dias de la semana
+
+        System.out.println("Registro de ventas semanales de la panaderia: ");
+
+        for (int i = 0; i < ventasSemana.length; i++) {
+            System.out.print("Ingrese las ventas del dia " + (i + 1) + ": "); // 100-700
+            ventasSemana[i] = scanner.nextDouble();
+        }
+        mostrarResumen(ventasSemana);
+
+        scanner.close();
+    }
+
+
+    public static void mostrarResumen(double[] ventas) {
+        double total = 0;
+        System.out.println("Ventas diarias: ");
+        for (int i = 0; i < ventas.length; i++) {
+            System.out.println("Dia " + (i + 1) + ": " + ventas[i]);
+            // total = total + ventas[i]; // acumulando el total de las ventas
+            total += ventas[i]; // acumulando el total de las ventas
+        }
+        System.out.println("total de ventas: " + total); // 2800
     }
 }
+
+
