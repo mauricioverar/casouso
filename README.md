0b7786b483e96495e59ddd240c8f680306a6fe2e
Mauricio <stb539@gmail.com>
2025-06-28 11:05:34 -0400

    inicio

---
+++ README.md
@@ -0,0 +1,12 @@
+Caso de Uso
+Caso de uso: Una panadería quiere registrar las ventas del día (hasta 7 días) y calcular el total y promedio de ingresos semanales.
+
+Reglas para ejercicio
+Cada día, se ingresa el total de ventas en pesos.
+
+Al finalizar los 7 días:
+
+Mostrar cada venta por día.
+Calcular el total semanal.
+Calcular el promedio diario.
+Todo el cálculo debe estar en un método estático separado.
---
+++ src/App.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception { // metodo estático main de arranque
+        // throws Exception para manejar excepciones
+
+        Scanner scanner = new Scanner(System.in); // Crear un objeto Scanner para leer la entrada del usuario
+
+        double total; // 0.0
+
+        System.out.println("Hello, World!");// metodo estatico println de la clase System para imprimir en consola
+    }
+}
